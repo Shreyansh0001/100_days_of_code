58daf3453fdb7efe57552fa6198a332123b118e6
Shreyansh0001 <42891311+Shreyansh0001@users.noreply.github.com>
2020-09-09 02:49:56 +0530

    Update auto_uploader.sh
    This is bash script that is meant to be executed every day at 23:59 and it checks if any file is added to the local folder where git is initialized. If a new file is present then it uploads the file with a comment containing the current day date.

--- auto_uploader.sh
+++ auto_uploader.sh
@@ -5,9 +5,19 @@ git add .
 git commit -m”${message}”
 if [ n “$(git status — porcelain)” ];
 then
- echo “IT IS CLEAN”
+ echo “No changes found”
 else
  git status
+ #add your git repository URL here
+ git remote add origin (remote repository URL) 
  echo “Pushing data to remote server!!!”
- git push -u origin master
+ git push -f origin master
 fi
+#TO EXECUTE THE CODE AUTOMATICALLY EVERYDAY USE THE FOLLOWING COMMANDS IN YOUR TERMINAL(exclude the "#")
+
+#crontab -e
+#this will open the editor for crontab -> type the following command
+
+#59 23 * * * /home/your_username/run-me.sh >/dev/null 2>&1 
+#This will make the script at /home/your_username/run-me.sh run at 23 hours 59 minutes every day every week and every month
+#the >/dev/null 2>&1 command is to stop the notification mails
